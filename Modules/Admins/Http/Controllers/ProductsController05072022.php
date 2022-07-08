2022333c2fdd378248a41a93c479fc93f7ab0536
root <root@localhost.localdomain>
2022-07-08 20:09:53 +0700

    fix

---
+++ Modules/Admins/Http/Controllers/ProductsController05072022.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace Modules\Admins\Http\Controllers;
+
+use App\Helpers\Helpers;
+use App\Service\Admins\CategoryService;
+use App\Service\Admins\ProductService;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\MessageBag;
+use Modules\Admins\Http\Requests\Product\CreateRequest;
+use Modules\Admins\Http\Requests\Product\EditRequest;
+
+class ProductsController extends Controller
+{
+
+    private $categoryService;
+    private $productService;
+    private $type;
+
+    public function __construct(CategoryService $categoryService, ProductService $productService)
+    {
+        $this->categoryService = $categoryService;
+        $this->productService = $productService;
+        $this->type = [$this->categoryService::TYPE[1]];
+    }
+
+    /**
+     * Category index
+     * @method GET
+     */
+    public function index()
+    {
+        try {
+            $data['common'] = Helpers::titleAction([__('admins::layer.product.index.title'), __('admins::layer.product.index.title2')]);
+            $data['list'] = $this->productService->getList(['limit' => 10, 'type' => $this->type[0]]);
+            $data['category'] = $this->categoryService->getListMenu(['type' => $this->type, 'parent_id' => [(request()->has('category_id') ? request()->get('category_id') : '')]]);
+            return view('admins::products.index', ['data' => $data]);
+        } catch (\Exception $e) {
+            abort('500');
+        }
+    }
+
+    /**
+     * Category add
+     * @method GET
+     */
+    public function create()
+    {
+        try {
+            $data['common'] = Helpers::titleAction([__('admins::layer.product.add.title'), __('admins::layer.product.index.title2')]);
+            $data['category'] = $this->categoryService->getListMenu(['type' => $this->type, 'multi' => true]);
+            return view('admins::products.create', ['data' => $data]);
+        } catch (\Exception $e) {
+            abort('500');
+        }
+    }
+
+    /**
+     * Category store
+     * @method POST
+     */
+    public function store(CreateRequest $request)
+    {
+        try {
+            $_params = $request->all();
+            if ($this->productService->create($_params)) {
+                return redirect(route('admin.product.index'));
+            } else {
+                $errors = new MessageBag(['error' => __('admins::layer.notify.fail')]);
+                return back()->withInput($_params)->withErrors($errors);
+            }
+        } catch (\Exception $e) {
+            abort('500');
+        }
+    }
+
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function show($id)
+    {
+        return view('admins::show');
+    }
+
+    /**
+     * Category edit
+     * @method GET
+     */
+    public function edit($id)
+    {
+        try {
+            $data['common'] = Helpers::titleAction([__('admins::layer.product.edit.title'), __('admins::layer.product.index.title2')]);
+            $data['detail'] = $this->productService->findById($id);
+            if (empty($data['detail']->id)) return abort(404);
+            $data['category'] = $this->categoryService->getListMenu(['type' => $this->type, 'parent_id' => [$data['detail']->category_id], 'multi' => true]);
+            return view('admins::products.edit', ['data' => $data]);
+        } catch (\Exception $e) {
+            return !empty($e->getMessage()) ? abort('500') : abort(404);
+        }
+    }
+
+    /**
+     * Category update
+     * @method POST
+     */
+    public function update(EditRequest $request, $id)
+    {
+        try {
+            $data['detail'] = $this->productService->findById($id);
+            if (empty($data['detail']->id)) return abort(404);
+            $_params = $request->all();
+            if ($this->productService->update($_params, $id)) {
+                session()->flash('success', __('admins::layer.notify.success'));
+                return redirect(route('admin.product.index', ['page' => !empty($_GET['page']) ? $_GET['page'] : 1]));
+            } else {
+                $errors = new MessageBag(['error' => __('admins::layer.notify.fail')]);
+                return back()->withInput($_params)->withErrors($errors);
+            }
+        } catch (\Exception $e) {
+            return !empty($e->getMessage()) ? abort('500') : abort(404);
+        }
+    }
+
+    /**
+     * Category status
+     * @method GET
+     */
+    public function status($id, $status)
+    {
+        try {
+            if ($this->productService->updateStatus($id, $status)) {
+                session()->flash('success', __('admins::layer.notify.success'));
+            } else {
+                session()->flash('error', __('admins::layer.notify.fail'));
+            }
+            return back();
+        } catch (\Exception $e) {
+            abort('500');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Renderable
+     */
+    public function actionIndex()
+    {
+        try {
+            $data = \request()->all();
+            if (empty($data['check']) || empty($data['action'])) {
+                session()->flash('error', __('admins::layer.notify.fail'));
+            } else {
+                $flag = true;
+                if ($data['action'] == 1) {
+                    $flag = $this->productService->destroyAll($data);
+                }
+
+                if ($flag) {
+                    session()->flash('success', __('admins::layer.notify.success'));
+                } else {
+                    session()->flash('error', __('admins::layer.notify.fail'));
+                }
+            }
+            return back();
+        } catch (\Exception $e) {
+            abort('500');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Renderable
+     */
+    public function destroy($id)
+    {
+        try {
+            if ($this->productService->destroy($id)) {
+                session()->flash('success', __('admins::layer.notify.success'));
+            } else {
+                session()->flash('error', __('admins::layer.notify.fail'));
+            }
+            return back();
+        } catch (\Exception $e) {
+            abort('500');
+        }
+    }
+
+}
---
+++ Modules/Clients/Http/Controllers/CategoriesControllerBak05072022.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Modules\Clients\Http\Controllers;
+
+use App\Service\Clients\AdvertisementService;
+use App\Service\Clients\ClientCategoryService;
+use App\Service\Clients\ClientPostService;
+use App\Service\Clients\ClientProductService;
+use App\Service\Clients\SettingService;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use App\Model\Category;
+use App\Model\Post;
+use App\Helpers\Helpers;
+use Illuminate\Support\Facades\View;
+
+class CategoriesController extends Controller
+{
+
+    private $clientSettingService;
+    private $clientAdvService;
+    private $clientCategoryService;
+    private $clientProductService;
+    private $clientPostService;
+    private $setting;
+
+    public function __construct(SettingService $clientSettingService,
+                                AdvertisementService $clientAdvService,
+                                ClientCategoryService $clientCategoryService,
+                                ClientProductService $clientProductService,
+                                ClientPostService $clientPostService
+    )
+    {
+        $this->clientSettingService = $clientSettingService;
+        $this->clientAdvService = $clientAdvService;
+        $this->clientCategoryService = $clientCategoryService;
+        $this->clientProductService = $clientProductService;
+        $this->clientPostService = $clientPostService;
+
+        $this->setting = $this->clientSettingService->findFirst();
+        View::share('data_common', [
+            'logo' => $this->clientAdvService->findByLogo(),
+            'setting' => $this->setting,
+            'category_list' => $this->clientCategoryService->getListMenu(['multi' => 1]),
+            'top_products' => $this->clientProductService->getListHome(['limit' => 8])
+        ]);
+    }
+
+    public function index($slug)
+    {
+        try {
+            $data['category'] = $this->clientCategoryService->findBySlug($slug);
+            if (empty($data['category']->id)) abort(404);
+            $data['setting'] = $this->setting;
+            $cateMulti = $this->clientCategoryService->multiCate($data['category']->id);
+
+            if ($data['category']->type == 'product') {
+                $data['list'] = $this->clientProductService->getListByCate(['cate_multi' => $cateMulti, 'limit' => 20]);
+
+                if (empty($data['category']->title_seo)) $data['category']->title_seo = "Giá " . $data['category']->title . " tốt nhất tháng " . date("m/Y", time());
+                if (empty($data['category']->meta_des)) {
+                    $des = !empty($data['category']->description) ? $data['category']->description : "";
+                    $data['category']->meta_des = Helpers::shortDesc($des, 150);
+                }
+                $data['common'] = Helpers::metaHead($data['category']);
+
+                return view('clients::products.index', ['data' => $data]);
+            } else {
+                if (empty($data['category']->title_seo)) $data['category']->title_seo = "Thông tin hữu ích về " . $data['category']->title . " - " . @ucfirst($_SERVER["HTTP_HOST"]);
+                if (empty($data['category']->meta_des)) {
+                    $des = !empty($data['category']->description) ? $data['category']->description : "";
+                    $data['category']->meta_des = Helpers::shortDesc($des, 150);
+                }
+                $data['common'] = Helpers::metaHead($data['category']);
+
+                $data['list'] = $this->clientPostService->getListByCate(['cate_multi' => $cateMulti, 'limit' => 15]);
+                if (count($data['list']) == 1 && empty($_GET['page'])) {
+                    return redirect(route('client.post.show', ['slug' => $data['list'][0]->slug]));
+                }
+
+                return view('clients::posts.index', ['data' => $data]);
+            }
+        } catch (\Exception $e) {
+            if (empty($e->getMessage())) abort(404); else abort('500');
+        }
+    }
+
+    public function search()
+    {
+        try {
+            $data['setting'] = $this->setting;
+            $data['common'] = Helpers::metaHead($data['setting']);
+            $data['search_title'] = !empty($_GET['key']) ? 'Tiếp tục mua hàng' : 'Tìm kiếm';
+            $keyword = !empty($_GET['keyword']) ? $_GET['keyword'] : '';
+            $data['list'] = $this->clientProductService->getListByCateSearch(['keyword' => $keyword, 'limit' => 20]);
+
+            return view('clients::products.index', ['data' => $data]);
+        } catch (\Exception $e) {
+            Helpers::pre($e->getMessage());
+            if (empty($e->getMessage())) abort(404); else abort('500');
+        }
+    }
+}
