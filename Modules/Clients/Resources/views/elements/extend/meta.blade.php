7010f4530956e887048fc3252c7cb2e2cc43bc1d
nguyentrongchinh1997 <chinh20091997@gmail.com>
2023-06-26 11:28:59 +0700

    fix seo

--- Modules/Clients/Resources/views/elements/extend/meta.blade.php
+++ Modules/Clients/Resources/views/elements/extend/meta.blade.php
@@ -4,13 +4,13 @@
 <link type="image/x-icon" href="{{ asset('/static/client/images/favicon.jpeg') }}" rel="icon">
 <link type="image/x-icon" href="{{ asset('/static/client/images/favicon.jpeg') }}" rel="shortcut icon">
 @include('feed::links')
-<title>{{ !empty($data['common']['title_seo']) ? $data['common']['title_seo'] : '' }}</title>
-<meta name="keywords" content="{{ !empty($data['common']['meta_key']) ? $data['common']['meta_key'] : '' }}"/>
-<meta name="description" content="{{ !empty($data['common']['meta_des']) ? $data['common']['meta_des'] : '' }}"/>
+<title>{{ !empty($data_common['setting']->title_seo) ? $data_common['setting']->title_seo : '' }}</title>
+<meta name="keywords" content="{{ !empty($data_common['setting']->meta_key) ? $data_common['setting']->meta_key : '' }}"/>
+<meta name="description" content="{{ !empty($data_common['setting']->meta_des) ? $data_common['setting']->meta_des : '' }}"/>
 
 <?php
-$title = !empty($data['detail']->title) ? $data['detail']->title : (!empty($data['common']['title_seo']) ? $data['common']['title_seo'] : '');
-$description = !empty($data['detail']->description) ? $data['detail']->description : (!empty($data['common']['meta_des']) ? $data['common']['meta_des'] : '');
+$title = !empty($data['detail']->title) ? $data['detail']->title : (!empty($data_common['setting']->title_seo) ? $data_common['setting']->title_seo : '');
+$description = !empty($data['detail']->description) ? $data['detail']->description : (!empty($data_common['setting']->meta_des) ? $data_common['setting']->meta_des : '');
 $img = '';
 if (!empty($data['detail']->id)) {
     $img = \App\Helpers\Helpers::renderThumb($data['detail']->thumbnail, 'share_fb');
